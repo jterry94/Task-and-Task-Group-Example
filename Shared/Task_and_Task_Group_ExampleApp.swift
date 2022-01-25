0442656e5148d4d9f0f1c27e0ccd799bae372a66
jterry94 <terryj@iit.edu>
2022-01-25 09:42:07 -0600

    Initial Commit

---
+++ Shared/Task_and_Task_Group_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_and_Task_Group_ExampleApp.swift
+//  Shared
+//
+//  Created by Jeff_Terry on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_and_Task_Group_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
